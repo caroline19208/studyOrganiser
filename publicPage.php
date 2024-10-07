27f3fc3c7e62792d65610b4beac08ee78b33b03e
livantik <chin.cmy@oundleschool.org.uk>
2024-10-07 10:07:14 +0100

    login form skeleton

---
+++ publicPage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Public page</title>
+    
+</head>
+<body>
+<form action="login.php">
+  Username <input type="text" name="forename"><br>
+  Password <input type="password" name="passwd"><br>
+  <br>
+
+  <input type="submit" value="Login">
+</form>
+   
+</body>
+</html>
